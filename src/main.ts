919bbe72dc8d439c2becacea6554aada29497ad6
leary423 <leary620@163.com>
2023-09-12 10:22:39 +0800

    add antd

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import Antd from "ant-design-vue";
+import router from "./router";
+import "./design/main.css";
+import "./design/app.less";
 
 createApp(App).mount('#app')
+
+const app = createApp(App);
+
+app.use(router);
+app.use(Antd);
+app.mount("#app");
