2cab73aacb16df50cae9c233ab89af6380e66274
leary423 <leary620@163.com>
2023-09-13 11:03:56 +0800

    change pic code

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -33,17 +33,17 @@
                 </a-form-item>
                 <a-form-item name="code" >
                   <div class="flex">
-                    <a-input class="w-[302px] mr-[10px]" v-model:value="formPwdData.code" placeholder="请输入校验码" allow-clear autocomplete="off" />
-                    <CaptchaInput ></CaptchaInput>
+                    <a-input @blur="checkCode" class="w-[302px] mr-[10px]" v-model:value="formPwdData.code" placeholder="请输入校验码" allow-clear autocomplete="off" />
+                    <CaptchaInput ref="captchaRef"></CaptchaInput>
                   </div>
                 </a-form-item>
               </a-form>
             </a-tab-pane>
           </a-tabs>
           <div class="absolute bottom-[54px] text-center w-[482px]">
-            <a-radio v-model:checked="isChecked">我已阅读并同意<label class="text-[#017AFF]">服务条款、隐私政策</label></a-radio>
+            <a-radio v-model:checked="isChecked"></a-radio>未注册手机号验证后将自动创建账号，登录即代表您已同意<label class="text-[#017AFF]">服务条款、隐私政策</label>
             <a-button type="primary" class="ant-btn-l w-full my-[20px]">{{ isLogin?'登录':'注册' }}</a-button>
-            <div class="text-[#484FFF] cursor-pointer" @click="goRegister">{{ isLogin?'注册账户':'已有账号登录' }}</div>
+            <!-- <div class="text-[#484FFF] cursor-pointer" @click="goRegister">{{ isLogin?'注册账户':'已有账号登录' }}</div> -->
           </div>
         </div>
     </div>
@@ -58,6 +58,7 @@ import CaptchaInput from '@/components/CaptchaInput.vue';
 const activeKey = ref("1");
 const isChecked = ref(false);
 const isLogin = ref(true);
+const captchaRef = ref()
 const formNoteData = reactive({
   mobile: '',
   code: '',
@@ -89,6 +90,10 @@ const formRules = computed(() => {
   };
 });
 
+const checkCode = () => {
+  console.log("formPwdData.code:",formPwdData.code)
+  console.log("identifyCode:",captchaRef.value?.identifyCode)
+}
 const goRegister = () => {
   isLogin.value = !isLogin.value;
 }
