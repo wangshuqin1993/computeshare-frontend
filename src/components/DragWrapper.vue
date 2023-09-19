bdfb46808a447973f58e057c4dc509934096dd2a
leary423 <leary620@163.com>
2023-09-19 16:52:54 +0800

    add check login

--- src/components/DragWrapper.vue
+++ src/components/DragWrapper.vue
@@ -6,91 +6,91 @@
         <div ref="moveDiv"
              @mousedown="mousedownFn($event)"
              :class="{'handler_ok_bg': confirmSuccess}"
-             class="handler handler_bg"
-             @reset="reset"></div>
+             class="handler handler_bg"></div>
     </div>
 </template>
 
-<script>
-export default {
-    data(){
-        return {
-            beginClientX:0,           /*距离屏幕左端距离*/
-            mouseMoveStata:false,     /*触发拖动状态  判断*/
-            maxwidth:500,               /*拖动最大宽度，依据滑块宽度算出来的*/
-            confirmWords:'拖动滑块验证',   /*滑块文字*/
-            confirmSuccess:false           /*验证成功判断*/
-        }
-    },
-    mounted(){
-        this.maxwidth = this.$refs.dragDiv.clientWidth - this.$refs.moveDiv.clientWidth
-        document.getElementsByTagName('html')[0].addEventListener('mousemove',this.mouseMoveFn)
-        document.getElementsByTagName('html')[0].addEventListener('mouseup',this.moseUpFn)
-    },
-    methods: {
-        mousedownFn:function (e) {
-            if(!this.confirmSuccess){
-                e.preventDefault && e.preventDefault()   //阻止文字选中等 浏览器默认事件
-                this.mouseMoveStata = true
-                this.beginClientX = e.clientX
-            }
-        },
-        //mousedoen 事件
-        successFunction(){
-            this.confirmSuccess = true
-            this.confirmWords = '验证通过'
-            this.$emit('onValidation', true)
-            if(window.addEventListener){
-                document.getElementsByTagName('html')[0].removeEventListener('mousemove',this.mouseMoveFn)
-                document.getElementsByTagName('html')[0].removeEventListener('mouseup',this.moseUpFn)
-            }else document.getElementsByTagName('html')[0].removeEventListener('mouseup',()=>{})
-            document.getElementsByClassName('drag_text')[0].style.color = '#fff'
-            document.getElementsByClassName('handler')[0].style.left = this.maxwidth + 'px'
-            document.getElementsByClassName('drag_bg')[0].style.width = this.maxwidth + 'px'
-        },
-        //验证成功函数
-        mouseMoveFn(e){
-            if(this.mouseMoveStata){
-                let width = e.clientX - this.beginClientX
-                if(width > 0 && width <= this.maxwidth) {
-                    document.getElementsByClassName('handler')[0].style.left = width + 'px'
-                    document.getElementsByClassName('drag_bg')[0].style.width = width + 'px'
-                }else if(width > this.maxwidth) this.successFunction()
-            }
-        },
-        //mousemove事件
-        moseUpFn(e){
-            this.mouseMoveStata = false
-            var width = e.clientX - this.beginClientX
-            if(width<this.maxwidth){
-                document.getElementsByClassName('handler')[0].style.left = 0 + 'px'
-                document.getElementsByClassName('drag_bg')[0].style.width = 0 + 'px'
-            }
-        },
-        reset(){
-            this.beginClientX = 0
-            this.maxwidth = this.$refs.dragDiv.clientWidth - this.$refs.moveDiv.clientWidth
-            this.mouseMoveStata = false
-            this.confirmSuccess= false
-            document.getElementsByClassName('drag_text')[0].style.color = 'rgba(0, 0, 0, 0.88)';
-            document.getElementsByClassName('handler')[0].style.left = 0 + 'px'
-            document.getElementsByClassName('drag_bg')[0].style.width = 0 + 'px'
-            this.confirmWords = '拖动滑块验证'
-            console.log('reset重置滑动条')
-            document.getElementsByTagName('html')[0].addEventListener('mousemove',this.mouseMoveFn)
-            document.getElementsByTagName('html')[0].addEventListener('mouseup',this.moseUpFn)
-        }
-    },
-    expose: ['reset', 'confirmSuccess'],
-    beforeDestory(){
-        document.getElementsByTagName('html')[0].removeEventListener('mousemove')
-        document.getElementsByTagName('html')[0].removeEventListener('mouseup')
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+const beginClientX = ref(0);           /*距离屏幕左端距离*/
+const mouseMoveStata = ref(false);     /*触发拖动状态  判断*/
+const maxwidth = ref(500);               /*拖动最大宽度，依据滑块宽度算出来的*/
+const confirmWords = ref('拖动滑块验证');   /*滑块文字*/
+const confirmSuccess = ref(false);          /*验证成功判断*/
+const dragDiv = ref();     
+const moveDiv = ref();
+
+onMounted(() =>{
+    maxwidth.value = dragDiv.value.clientWidth - moveDiv.value.clientWidth
+    document.getElementsByTagName('html')[0].addEventListener('mousemove',mouseMoveFn)
+    document.getElementsByTagName('html')[0].addEventListener('mouseup',moseUpFn)
+})
+onBeforeUnmount(() => {
+  document.getElementsByTagName('html')[0].removeEventListener('mousemove', mouseMoveFn)
+  document.getElementsByTagName('html')[0].removeEventListener('mouseup', moseUpFn)
+})
+const mousedownFn = (e) =>{
+  if(!confirmSuccess.value){
+    e.preventDefault && e.preventDefault()   //阻止文字选中等 浏览器默认事件
+    mouseMoveStata.value = true
+    beginClientX.value = e.clientX
+  }
+}
+//mousedoen 事件
+const successFunction = () => {
+    confirmSuccess.value = true
+    confirmWords.value = '验证通过'
+    // this.$emit('onValidation', true)
+  if (window.addEventListener) {
+    document.getElementsByTagName('html')[0].removeEventListener('mousemove', mouseMoveFn)
+    document.getElementsByTagName('html')[0].removeEventListener('mouseup', moseUpFn)
+  } else {
+    document.getElementsByTagName('html')[0].removeEventListener('mouseup', () => { })
+  } 
+
+  (document.getElementsByClassName('drag_text')[0] as HTMLElement).style.color = '#fff';
+  (document.getElementsByClassName('handler')[0] as HTMLElement).style.left = maxwidth.value + 'px';
+  (document.getElementsByClassName('drag_bg')[0] as HTMLElement).style.width = maxwidth.value + 'px';
+}
+//验证成功函数
+const mouseMoveFn = (e) => {
+    if(mouseMoveStata.value){
+        let width = e.clientX - beginClientX.value
+        if(width > 0 && width <= maxwidth.value) {
+          (document.getElementsByClassName('handler')[0] as HTMLElement).style.left = width + 'px';
+          (document.getElementsByClassName('drag_bg')[0] as HTMLElement).style.width = width + 'px';
+        }else if(width > maxwidth.value) successFunction()
     }
 }
+//mousemove事件
+const moseUpFn = (e) => {
+    mouseMoveStata.value = false
+    var width = e.clientX - beginClientX.value
+    if(width<maxwidth.value){
+      (document.getElementsByClassName('handler')[0] as HTMLElement).style.left = 0 + 'px';
+      (document.getElementsByClassName('drag_bg')[0] as HTMLElement).style.width = 0 + 'px';
+    }
+}
+const dragInit = () => {
+  beginClientX.value = 0;
+  confirmSuccess.value = false;
+  mouseMoveStata.value = false;
+  confirmWords.value = '拖动滑块验证';
+  (document.getElementsByClassName('drag_text')[0] as HTMLElement).style.color = 'rgba(0, 0, 0, 0.88)';
+  (document.getElementsByClassName('handler')[0] as HTMLElement).style.left = 0 + 'px';
+  (document.getElementsByClassName('drag_bg')[0] as HTMLElement).style.width = 0 + 'px';
+
+    document.getElementsByTagName('html')[0].addEventListener('mousemove',mouseMoveFn)
+    document.getElementsByTagName('html')[0].addEventListener('mouseup',moseUpFn)
+}
+defineExpose({
+  dragInit,confirmSuccess
+})
 </script>
 
 
-<style scoped>
+<style lang="less" scoped>
 .drag-wrapper{
     width: 500px;
     border: 1px solid #D9D9D9;
