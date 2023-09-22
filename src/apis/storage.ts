af30471f331b65dac27b1d92d8779ad6ba11aef4
Dennisic\Chen <cgyforwork@foxmail.com>
2023-09-22 17:10:24 +0800

    feat: storage apis

--- src/apis/storage.ts
+++ src/apis/storage.ts
@@ -1,1 +1,34 @@
 import httpRequest from "@/request";
+
+// 列表查询
+export function apiStorageList(parentId: string) {
+    return httpRequest({
+        url: `/v1/storage?parentId=${parentId}`,
+        method: "get",
+    });
+}
+
+// 删除
+export function apiDelStorage(ids: [string]) {
+    return httpRequest({
+        url: `/v1/storage?ids=${ids}`,
+        method: "delete",
+    });
+}
+
+// 上传
+export function apiUploadStorage(params:any) {
+    return httpRequest({
+        url: `/v1/storage/upload`,
+        method: "post",
+        data: params
+    });
+}
+
+// 下载
+export function apiDownloadStorage(id: string) {
+    return httpRequest({
+        url: `/v1/storage/download?id=${id}`,
+        method: "post",
+    });
+}
