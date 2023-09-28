6a5c22dcd65b86ccb63a17725c0a5c1bfdebe952
mohaijiang <mohaijiang110@163.com>
2023-09-28 15:37:08 +0800

    动态websocket地址

--- src/components/useWebsocket.vue
+++ src/components/useWebsocket.vue
@@ -1,7 +1,7 @@
 <template>
     <div ref="terminal"></div>
 </template>
-  
+
 <script>
 import "xterm/dist/xterm.css";
 import "xterm/dist/addons/fullscreen/fullscreen.css";
@@ -12,7 +12,7 @@ import * as fit from "xterm/lib/addons/fit/fit";
 import * as fullscreen from "xterm/lib/addons/fullscreen/fullscreen";
 import * as webLinks from "xterm/lib/addons/webLinks/webLinks";
 import * as attach from "xterm/lib/addons/attach/attach";
-  
+
   export default {
     name: "system",
     created() {
@@ -23,11 +23,16 @@ import * as attach from "xterm/lib/addons/attach/attach";
       Terminal.applyAddon(fit);
       Terminal.applyAddon(fullscreen);
       Terminal.applyAddon(webLinks);
-  
+
       const terminal = new Terminal();
       // instanceId 实例id，路由中取
-      // wss://computeshare.hamster.newtouch.com/v1/vm/terminal?instanceId=${instanceId}
-      const ws = new WebSocket( `wss://computeshare.hamster.newtouch.com/v1/vm/terminal?instanceId=${instanceId}`);
+      // 获取当前浏览器地址
+      const currentLocation = window.location;
+
+      // 将当前协议（http/https）替换为WebSocket协议（ws/wss）
+      const wsProtocol = currentLocation.protocol === 'https:' ? 'wss:' : 'ws:';
+
+      const ws = new WebSocket( `${wsProtocol}//${currentLocation.host}/api/v1/vm/terminal?instanceId=${instanceId}`);
       ws.onclose = function() {
         console.log("服务器关闭了连接");
       };
