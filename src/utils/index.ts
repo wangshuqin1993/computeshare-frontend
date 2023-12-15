fb33a16e983ae84fe9ed84ed72c158efa0ac9140
Dennisic\Chen <cgyforwork@foxmail.com>
2023-12-15 14:37:56 +0800

    feat: finish develop modal

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+import { message } from 'ant-design-vue'
 // 公共回调
 // 下载 str:下载的内容  name:下载文件的名称  format:下载文件的格式
 export const downloadRequest = (str: string, name: string) => {
@@ -42,4 +43,42 @@ export function getfilesize(size) {//把字节转换成正常文件大小
   if (size < Math.pow(num, 4))
     return (size / Math.pow(num, 3)).toFixed(2) + "G"; //G
   return (size / Math.pow(num, 4)).toFixed(2) + "T"; //T
+}
+
+// 复制文本
+export const copyToClipboard = (text:string) => {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    message.success("copy success");
+  }, function(err) {
+    message.error("copy failed");
+  });
+}
+
+export const fallbackCopyTextToClipboard = async(_items:string) => {
+  // 存储传递过来的数据
+  let OrderNumber = _items;
+  // 创建一个input 元素
+  // createElement() 方法通过指定名称创建一个元素
+  let newInput = document.createElement("input");
+  // 讲存储的数据赋值给input的value值
+  newInput.value = OrderNumber;
+  // appendChild() 方法向节点添加最后一个子节点。
+  document.body.appendChild(newInput);
+  // 选中input元素中的文本
+  // select() 方法用于选择该元素中的文本。
+  newInput.select();
+  // 执行浏览器复制命令
+  try {
+    //  execCommand方法是执行一个对当前文档，当前选择或者给出范围的命令
+    await document.execCommand('Copy') // 执行浏览器复制命令
+    // 清空输入框
+    newInput.remove();
+    message.success("copy success");
+  } catch {
+    message.error("copy failed");
+  }
 }
--- src/views/developer/index.vue
+++ src/views/developer/index.vue
@@ -4,17 +4,25 @@
         <div class="bg-[#FFFFFF] rounded-[2px] px-[40px] py-[20px]">
             <a-button @click="checkKey">查看密钥</a-button>
             <a-button @click="DelelteKey">删除密钥</a-button>
+            <a-button @click="showKeepModalFn">保管密钥</a-button>
         </div>
     </div>
-    <checkOrDelKeyModal :visibleMobile="visibleMobile" :title="title" @closeCheckKeyModal="closeCheckKeyModal" @showKeyModal="showKeyModal" @delKeyFn="delKeyFn"/>
+    <checkOrDelKeyModal v-if="visibleMobile" :visibleMobile="visibleMobile" :title="title" @closeCheckKeyModal="closeCheckKeyModal" @showKeyModalFn="showKeyModalFn" @delKeyFn="delKeyFn"/>
+    <showKeyModal :showKeepKeyVisible="showKeepKeyVisible" :keyInfo="keyInfo" @closeKeyModal="closeKeyModal"/>
 </template>
 <script setup lang="ts">
 import { computed, onMounted, reactive, ref } from 'vue';
 import Header from "@/components/Header.vue";
-import checkOrDelKeyModal from './components/checkOrDelKeyModal.vue'
+import checkOrDelKeyModal from './components/checkOrDelKeyModal.vue';
+import showKeyModal from './components/showKeyModal.vue';
 
 const visibleMobile = ref(false)
+const showKeepKeyVisible = ref(false)
 const title = ref('')
+const keyInfo = ref({
+    access_key: 'ptif2233yntj8r2ptife',
+    secret_key: 'ptif2233yntj8r2ptife'
+})
 
 const closeCheckKeyModal = ()=>{
     visibleMobile.value = false
@@ -31,13 +39,23 @@ const DelelteKey = ()=>{
 }
 
 // 打开密钥弹框
-const showKeyModal = ()=>{
+const showKeyModalFn = ()=>{
     visibleMobile.value = false
 }
 
 // 删除密钥
 const delKeyFn = ()=>{
 
+}
+
+// 保管密钥弹框
+const showKeepModalFn = ()=>{
+    showKeepKeyVisible.value = true
+}
+
+// 关闭密钥弹框
+const closeKeyModal = ()=>{
+    showKeepKeyVisible.value = false
 }
 
 </script>
