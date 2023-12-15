52503bfe1bab6d025b902544f0262a8d69cb13f1
Dennisic\Chen <cgyforwork@foxmail.com>
2023-12-15 17:06:04 +0800

    feat: finish developer page

--- src/views/developer/index.vue
+++ src/views/developer/index.vue
@@ -1,14 +1,78 @@
 <template>
     <Header/>
-    <div class="bg-[#FFFFFF] rounded-[2px] p-[20px] m-[20px]">
+    <div class="rounded-[2px] p-[20px] m-[20px] bg-[#FFFFFF] scroll-max-h overflow-y-auto">
         <!-- 存储信息 -->
-        <div class="p-[20px] border border-solid border-[#E9E9E9] rounded-[4px]">
-            <p class="text-[18px] font-medium">文件存储SDK</p>
-            <p class="text-[14px] text-[#8C8C8C] font-normal">使用访问密钥从SDK以编程方式调用文件存</p>
+        <div class="border border-solid border-[#E9E9E9] rounded-[4px]">
+            <div class="p-[20px]">
+                <p class="text-[18px] font-medium mb-[5px]">文件存储SDK</p>
+                <p class="text-[14px] text-[#8C8C8C] font-normal">使用访问密钥从SDK以编程方式调用文件存 </p>
+            </div>
+            <div class="w-[100%] mb-[18px] line"></div>
+            <div class="p-[20px]">
+                <p class="text-[16px] font-medium text-[#595959]">下载路径</p>
+                <div class="text-[14px] font-normal mt-[14px]">
+                    <div>
+                        <span class="w-[45px] inline-block">Go：</span>
+                        <span>https://github.com/aws/aws-sdk-go</span>
+                    </div>
+                    <div>
+                        <span class="w-[45px] inline-block">Java：</span>
+                        <span>https://github.com/aws/aws-sdk-java</span>
+                    </div>
+                </div>
+                <div class="w-[100%] my-[18px] line"></div>
+                <p class="text-[16px] font-medium text-[#595959] mb-[10px]">配置凭证</p>
+                <p class="text-[14px] font-medium">您可以使用任何文本编辑器在您的主目录中创建一个。将以下内容添加到您的凭据文件中，并将 YOUR_ACCESS_KEY 和替换 YOUR_SECRET_KEY 为您的凭据。</p>
+                <div class="rounded-[4px] bg-[#FAFBFF] p-[20px] mt-[14px] mb-[20px] text-[14px]">
+                    <p>
+                        <span class="text-[#C8AD26]">host_base = </span>
+                        <span class="text-[#60A210]">computeshare.newtouch.com:8333</span>
+                    </p>
+                    <p class="py-[10px]">
+                        <span class="text-[#C8AD26]">access_key = </span>
+                        <span class="text-[#60A210]">YOUR_ACCESS_KEY</span>
+                    </p>
+                    <p>
+                        <span class="text-[#C8AD26]">secret_key = </span>
+                        <span class="text-[#60A210]">YOUR_SECRET_KEY</span>
+                    </p>
+                </div>
+            </div>
         </div>
         <!-- 密钥信息 -->
-        <div class="p-[20px]">
-
+        <div class="border border-solid border-[#E9E9E9] rounded-[4px] mt-[20px]">
+            <div class="p-[20px]">
+                <p class="text-[18px] font-medium mb-[5px]">访问密钥</p>
+                <p class="text-[14px] text-[#8C8C8C] font-normal">使用访问密钥从SDK以编程方式调用文件存储 </p>
+            </div>
+            <div class="w-[100%] line"></div>
+            <div class="p-[20px]">
+                <div class="w-[100%] flex justify-end mb-[20px]">
+                    <a-button type="primary" class="w-[156px] h-[38px] text-[14px] text-center rounded-[5px] font-normal" @click="createNewKey">创建密钥</a-button>
+                </div>
+                <a-table :columns="secretKeyColumns" :data-source="secretKeyList">
+                    <template #bodyCell="{ column, text, record }">
+                        <template v-if="column.key === 'access_key'">
+                            <!-- {{text[0]}} -->
+                            <span>{{ getPonitStr(text,2,2) }}</span>
+                        </template>
+                        <template v-if="column.key === 'secret_key'">
+                            <span>{{ getPonitStr(text,2,2) }}</span>
+                        </template>
+                        <template v-if="column.key === 'action'">
+                            <a-tooltip placement="left" color="#FFFFFF">
+                                <template #title>
+                                    <div class="text-[14px] w-[120px] px-[20px]">
+                                        <div @click="checkKey(record)" class="cursor-pointer text-[#000] hover:text-[#484FFF]">查看密钥</div>
+                                        <div @click="DelelteKey(record)" class="cursor-pointer text-[#000] mt-[10px] hover:text-[#484FFF]">删除</div>
+                                    </div>
+                                </template>
+                                <img src="@/assets/images/more-vertical.svg" class="h-[26px] cursor-pointer" />
+                            </a-tooltip>
+                        </template>
+                    </template>
+                </a-table>
+            </div>
         </div>
     </div>
     <checkOrDelKeyModal v-if="visibleMobile" :visibleMobile="visibleMobile" :title="title" @closeCheckKeyModal="closeCheckKeyModal" @showKeyModalFn="showKeyModalFn" @delKeyFn="delKeyFn"/>
@@ -19,6 +83,7 @@ import { computed, onMounted, reactive, ref } from 'vue';
 import Header from "@/components/Header.vue";
 import checkOrDelKeyModal from './components/checkOrDelKeyModal.vue';
 import showKeyModal from './components/showKeyModal.vue';
+import { getPonitStr } from '@/utils/index'
 
 const visibleMobile = ref(false)
 const showKeepKeyVisible = ref(false)
@@ -27,6 +92,37 @@ const keyInfo = ref({
     access_key: 'ptif2233yntj8r2ptife',
     secret_key: 'ptif2233yntj8r2ptife'
 })
+
+const secretKeyColumns = [
+  {
+    title: 'access_key',
+    dataIndex: 'access_key',
+    key: 'access_key',
+  },
+  {
+    title: 'secret_key',
+    dataIndex: 'secret_key',
+    key: 'secret_key',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'time',
+    key: ' time',
+  },
+  {
+    title: '',
+    key: 'action',
+    align: 'right'
+  },
+];
+
+const secretKeyList = ref([
+    {
+        access_key: 'mo11111111111111111111111111111111111111ng',
+        secret_key: 'Ab2222222222222222222222222222222222222234',
+        time: '2023.09.08 17:18'
+    }
+])
 
 const closeCheckKeyModal = ()=>{
     visibleMobile.value = false
@@ -62,7 +158,17 @@ const closeKeyModal = ()=>{
     showKeepKeyVisible.value = false
 }
 
+// 创建密钥
+const createNewKey = ()=>{
+
+}
+
 </script>
 <style lang="less" scoped>
-
+.line {
+  border-top: 1px solid #E9E9E9;
+}
+.scroll-max-h{
+  max-height: calc(100vh - 242px);
+}
 </style>
