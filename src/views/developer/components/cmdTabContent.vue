11bba3ae001fecf95155c9fc76334123f0570a8d
leary423 <leary620@163.com>
2023-12-29 14:29:22 +0800

    add develop tab

---
+++ src/views/developer/components/cmdTabContent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="p-[20px]">
+    <p class="text-[14px] text-[#8C8C8C] font-normal">使用访问密钥从SDK以编程方式调用文件存 </p>
+    <p class="title">安装工具</p>
+    <p class="title-sub">在终端执行以下命令安装一个免费的命令行工具</p>
+    <div class="div-bg text-[#E34594]">
+        sudo apt install s3cmd
+    </div>
+    <div class="w-[100%] my-[18px] line"></div>
+    <p class="title">配置凭证</p>
+    <p class="title-sub">在终端执行以下命令来更改配置文件的内容</p>
+    <div class="div-bg text-[#E34594]">
+        sudo apt install s3cmd
+    </div>
+    <p class="title-sub">将以下内容添加到文件中，并将 &lt;YOUR_ACCESS_KEY&gt;和&lt;YOUR_SECRET_KEY&gt;替换为您的凭据。</p>
+    <div class="div-bg">
+        <p>
+            <span class="text-[#C8AD26]">host_base = </span>
+            <span class="text-[#60A210]">computeshare.newtouch.com:8333</span>
+        </p>
+        <p class="py-[10px]">
+            <span class="text-[#C8AD26]">access_key = </span>
+            <span class="text-[#60A210]">&lt;YOUR_ACCESS_KEY&gt;</span>
+        </p>
+        <p>
+            <span class="text-[#C8AD26]">secret_key = </span>
+            <span class="text-[#60A210]">&lt;YOUR_SECRET_KEY&gt;</span>
+        </p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+.line {
+  border-top: 1px solid #E9E9E9;
+}
+.title{
+  @apply text-[16px] font-medium text-[#595959] mt-[20px];
+}
+.title-sub{
+  @apply text-[rgba(0,0,0,0.65)] text-[14px] font-medium mt-[10px];
+}
+.div-bg{
+  @apply rounded-[4px] bg-[#FAFBFF] p-[20px] mt-[14px] mb-[20px] text-[14px];
+}
+</style>
---
+++ src/views/developer/components/sdkTabContent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="p-[20px]">
+    <p class="text-[16px] font-medium text-[#595959]">下载路径</p>
+    <div class="text-[14px] font-normal mt-[14px]">
+        <div>
+            <span class="w-[45px] inline-block">Go：</span>
+            <span>https://github.com/aws/aws-sdk-go</span>
+        </div>
+        <div>
+            <span class="w-[45px] inline-block">Java：</span>
+            <span>https://github.com/aws/aws-sdk-java</span>
+        </div>
+    </div>
+    <div class="w-[100%] my-[18px] line"></div>
+    <p class="text-[16px] font-medium text-[#595959] mb-[10px]">配置凭证</p>
+    <p class="text-[14px] font-medium">您可以使用任何文本编辑器在您的主目录中创建一个。将以下内容添加到您的凭据文件中，并将 &lt;YOUR_ACCESS_KEY&gt; 和替换 &lt;YOUR_SECRET_KEY&gt; 为您的凭据。</p>
+    <div class="rounded-[4px] bg-[#FAFBFF] p-[20px] mt-[14px] mb-[20px] text-[14px]">
+        <p>
+            <span class="text-[#C8AD26]">host_base = </span>
+            <span class="text-[#60A210]">computeshare.newtouch.com:8333</span>
+        </p>
+        <p class="py-[10px]">
+            <span class="text-[#C8AD26]">access_key = </span>
+            <span class="text-[#60A210]">&lt;YOUR_ACCESS_KEY&gt;</span>
+        </p>
+        <p>
+            <span class="text-[#C8AD26]">secret_key = </span>
+            <span class="text-[#60A210]">&lt;YOUR_SECRET_KEY&gt;</span>
+        </p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
