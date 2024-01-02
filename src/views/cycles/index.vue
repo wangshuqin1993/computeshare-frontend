e9ae23875df871c53944bdd0eee5255cde789c6b
Dennisic\Chen <cgyforwork@foxmail.com>
2024-01-02 13:59:16 +0800

    feat: save code

--- src/views/cycles/index.vue
+++ src/views/cycles/index.vue
@@ -1,7 +1,12 @@
 <template>
-    cycles
+    <Header />
 </template>
 <script setup lang="ts">
+import { useRouter} from 'vue-router'
+import Header from "@/components/Header.vue";
+
+const router = useRouter()
+
 
 </script>
 <style lang="less" scoped>
