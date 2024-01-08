351d3b87a84359b0cf559604de9bee6b201ad2c1
leary423 <leary620@163.com>
2024-01-08 15:55:41 +0800

    add cycle

---
+++ src/views/cycles/components/payTabContent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="p-[50px]">
+    <div class="flex">
+      <div class="div-card">
+        <div class="title">50,000 Cycles</div>
+        <div class="sub-text">¥ 50</div>
+      </div>
+    </div>
+    <div class="bt-css"></div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+.div-card{
+  border-radius: 6px;
+  border: 2px solid #DCDFE6;
+  padding: 30px 50px;
+  text-align: center;
+  .title{
+    @apply text-[21px] font-semibold text-[#000000]
+  }
+  .sub-text{
+    @apply text-[18px] text-[#606266] mt-[16px];
+  }
+}
+.bt-css{
+  border-top: 1px solid #E9E9E9;
+}
+</style>
--- src/views/cycles/index.vue
+++ src/views/cycles/index.vue
@@ -1,17 +1,55 @@
 <template>
     <Header />
-    <div class="cursor-pointer" @click="goDetail">详情</div>
+    <div class="m-[20px]">
+      <div class="flex">
+        <div class="w-[70%]">
+          <div class="bg-[#FFFFFFFF] rounded-[2px] mb-[2px] h-[100px] px-[20px] flex justify-between items-center">
+            <div class="flex items-center">
+              <span class="text-[18px] font-medium">Cycles余额</span>
+              <span class="ml-[30px] text-[36px] font-semibold">0.00</span>
+            </div>
+            <div class="cursor-pointer text-[#484FFF] font-medium" @click="goDetail">充值记录</div>
+          </div>
+          <div class="bg-[#FFFFFFFF] rounded-[2px] mt-[20px] p-[20px] pl-0">
+            <a-tabs v-model:activeKey="activeKey">
+              <a-tab-pane key="1" tab="充值">
+                <payTabContent ></payTabContent>
+              </a-tab-pane>
+              <a-tab-pane key="2" tab="兑换">
+                <sdkTabContent></sdkTabContent>
+              </a-tab-pane>
+            </a-tabs>
+          </div>
+        </div>
+        <div class="w-0 flex-1 ml-[20px] bg-[#FFFFFFFF] rounded-[2px]">
+          123
+        </div>
+      </div>
+    </div>
 </template>
 <script setup lang="ts">
 import { useRouter} from 'vue-router'
 import Header from "@/components/Header.vue";
+import payTabContent from "./components/payTabContent.vue"
+import { ref } from 'vue';
 
 const router = useRouter()
+const activeKey = ref('1');
 
 const goDetail = () => {
   router.push('/dashboard/cyclesDetail')
 }
 </script>
 <style lang="less" scoped>
-
+:deep(.ant-tabs-top > .ant-tabs-nav){
+  margin-bottom: 0px !important;
+}
+:deep(.ant-tabs .ant-tabs-tab){
+  padding: 12px 30px;
+  font-size: 18px;
+}
+:deep(.ant-tabs .ant-tabs-tab.ant-tabs-tab-active .ant-tabs-tab-btn){
+  color: #262626;
+  font-weight: 600;
+}
 </style>
